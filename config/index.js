7133499cf8a66dbd4a75e68dad91e53967f6ef90
zhulijun <zhulijun@netwisdom.cn>
2018-06-11 16:58:34 +0800

    配置修改

--- config/index.js
+++ config/index.js
@@ -13,9 +13,9 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: 'dev.business.zanfin.com', // can be overwritten by process.env.HOST
+    host: '172.19.111.23', // can be overwritten by process.env.HOST
     // host: '192.168.1.33', // can be overwritten by process.env.HOST
-    port: '', // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 8082, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
     notifyOnErrors: true,
