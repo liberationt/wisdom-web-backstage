c4f3d6cb5efeef637d2441e5b3e36ffc40396b9c
liulili <liulili@netwisdom.cn>
2018-07-17 10:10:14 +0800

    校验

--- src/components/bigMoney/loan-test.vue
+++ src/components/bigMoney/loan-test.vue
@@ -48,7 +48,7 @@
                 :show-upload-list="false"
                 :format="['jpg','jpeg','png', 'gif']"
                 :on-format-error="handleFormatError1"
-                action=''>
+                <!-- action=''> -->
                   <Button type="ghost" icon="ios-cloud-upload-outline" style="margin-top:24px">选择</Button>
                 </Upload>
               </FormItem>
@@ -170,20 +170,20 @@ export default {
         productlogo: { required: true, message: '请上传产品LOGO', trigger: 'blur' },
         name: [
           { required: true, message: '请输入产品名称', trigger: 'blur' },
-          { max: 100, message: '产品名称最多输入100个字符', trigger: 'blur' },
-          { min: 2, message: '产品名称最少输入2个字符', trigger: 'blur' },
+          { max: 100, message: '产品名称最多输入50个字', trigger: 'blur' },
+          { min: 2, message: '产品名称最少输入1个字', trigger: 'blur' },
           {required: true, message: '请输入正确的产品名称', pattern: /^[\u4e00-\u9fa5a-zA-Z0-9]+$/, trigger: 'blur'}
         ],
         subtitle: [
           { required: true, message: '请输入产品副标题', trigger: 'blur' },
-          { max: 100, message: '产品副标题最多输入100个字符', trigger: 'blur' },
-          { min: 2, message: '产品副标题最少输入2个字符', trigger: 'blur' },
+          { max: 100, message: '产品副标题最多输入50个字', trigger: 'blur' },
+          { min: 2, message: '产品副标题最少输入1个字', trigger: 'blur' },
           {required: true, message: '请输入正确的产品名称', pattern: /^[\u4e00-\u9fa5a-zA-Z0-9]+$/, trigger: 'blur'}
         ],
         explain: [
           { required: true, message: '请输入产品说明', trigger: 'blur' },
-          { max: 100, message: '产品副标题最多输入100个字符', trigger: 'blur' },
-          { min: 2, message: '产品副标题最少输入2个字符', trigger: 'blur' },
+          { max: 100, message: '产品副标题最多输入50个字', trigger: 'blur' },
+          { min: 2, message: '产品副标题最少输入1个字符', trigger: 'blur' },
           {required: true, message: '请输入正确的产品名称', pattern: /^[\u4e00-\u9fa5a-zA-Z0-9]+$/, trigger: 'blur'}
         ],
         types: { required: true, message: '请选择额度类型', trigger: 'blur' },
@@ -596,7 +596,6 @@ export default {
     // 上传文件提示
     // 上传格式校验
     handleFormatError1 (file) {
-      alert(222)
       this.formCustom.productlogo = ''
       this.$Message.info("图片格式不正确,请上传正确的图片格式")
     },
