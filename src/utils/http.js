49fbeb7bd4dadd63bbdd50ec4b1b59bdc13d2db0
齐玉飞 <qiyufei@netwisdom.cn>
2018-07-21 04:33:20 +0800

    大网钱

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import utils from '../utils/utils'
+import {Modal} from 'iview'
 // import qs from 'qs';
 axios.defaults.timeout = 5000
 axios.defaults.baseURL = ''
@@ -41,13 +42,38 @@ axios.interceptors.response.use.bind(axios.interceptors.response)
 addResponseInterceptor(
   (response) => {
     // 在这里统一前置处理请求响应
-    if (Number(response.status) !== 200) {
+    if (Number(response.status) === 200) {
       // 全局notify有问题，还是自己处理吧
-      return Promise.reject(response.data)
+      // return Promise.reject(response.data)
+      // window.location.href = './'
+      // this.$router.push({ path: './' })
     }
     return Promise.resolve(response.data)
   },
   (error) => {
+    if (error.response) {
+      const title = '温馨提示';
+      const content = '<p>token过期请重新登录</p>'
+      switch (error.response.status) {
+        case 401:
+          // 返回 401 跳转到登录页面       
+          Modal.error({
+            title: title,
+            content: content,
+            onOk: () => {
+              localStorage.removeItem("lefthidden")
+              localStorage.removeItem("leftlist")
+              localStorage.removeItem("token")
+              localStorage.removeItem("userInfo")
+              localStorage.removeItem("headace")
+              localStorage.removeItem("sideleft")
+              utils.delCookie("user");
+              window.location.href = './'
+          }
+        })
+          break
+      }
+    }
     return Promise.reject(error || '出错了')
   }
 )
