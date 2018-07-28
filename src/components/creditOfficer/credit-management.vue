0b86ce1e1bc01a79279fd1abf7a3846d5df05954
liulili <liulili@netwisdom.cn>
2018-07-28 22:38:32 +0800

    枪弹侠管理

--- src/components/creditOfficer/credit-management.vue
+++ src/components/creditOfficer/credit-management.vue
@@ -5,8 +5,8 @@
         <span>管理首页&nbsp;>&nbsp;应用&nbsp;>&nbsp;百姓钱袋&nbsp;>&nbsp;信贷员管理</span>
       </p>
     </div>
-    <Tabs type="card" :animated="false">
-        <TabPane :label="label"  on-click	="labelll">
+    <Tabs type="card" :animated="false" @on-click="labell1">
+        <TabPane :label="label" >
             <div class="clearfix" >
             <div class="left">
             <Select v-model="model1" placeholder="姓名" style="width:100px">
@@ -16,8 +16,11 @@
             <Select v-model="model3" placeholder="所属状态" style="width:200px;margin-left:50px">
                 <Option v-for="item in status" :value="item.value" :key="item.value">{{ item.label }}</Option>
             </Select>
-            <Select v-model="model2" placeholder="所属区域" style="width:200px;margin-left:50px">
-                <Option v-for="item in cityType" :value="item.value" :key="item.value">{{ item.label }}</Option>
+            <Select v-model="model2" @on-change="citys" placeholder="请选择省" style="width:200px;margin-left:50px">
+                <Option v-for="item in cityType" :value="item.adcode" :key="item.adcode">{{ item.name }}</Option>
+            </Select>
+            <Select v-model="modell" @on-change="cityh" placeholder="请选择市" style="width:200px;margin-left:50px">
+                <Option v-for="item in cityTypel" :value="item.adcode" :key="item.adcode">{{ item.name }}</Option>
             </Select>
             </div>
             <Button class="right mr100" type="primary" icon="ios-search">查询</Button>
@@ -57,7 +60,7 @@
                 <Option v-for="item in status" :value="item.value" :key="item.value">{{ item.label }}</Option>
             </Select>
             <Select v-model="model2" placeholder="所属区域" style="width:200px;margin-left:50px">
-                <Option v-for="item in cityType" :value="item.value" :key="item.value">{{ item.label }}</Option>
+                <Option v-for="item in cityType1" :value="item.adcode" :key="item.adcode">{{ item.name }}</Option>
             </Select>
             </div>
             <Button class="right mr100" type="primary" icon="ios-search">查询</Button>
@@ -77,12 +80,12 @@
             </Select>
             <Input v-model="name" placeholder="请输入关键字" style="width: 150px"></Input>
             <Select v-model="model2" placeholder="所属区域" style="width:200px;margin-left:50px">
-                <Option v-for="item in cityType" :value="item.value" :key="item.value">{{ item.label }}</Option>
+                <Option v-for="item in cityType2" :value="item.adcode" :key="item.adcode">{{ item.name }}</Option>
             </Select>
             <Select v-model="model3" placeholder="上架状态" style="width:200px;margin-left:50px">
                 <Option v-for="item in status" :value="item.value" :key="item.value">{{ item.label }}</Option>
             </Select>
-            <Select v-model="model4" placeholder="排序" style="width:200px;margin-left:50px">
+            <Select v-model="model4" placeholder="服务介绍状态" style="width:200px;margin-left:50px">
                 <Option v-for="item in status" :value="item.value" :key="item.value">{{ item.label }}</Option>
             </Select>
             </div>
@@ -92,7 +95,7 @@
             <Table border :columns="columns10" :data="data9"></Table>
             </div>
             <div class="tr mt15">
-            <Page :total="100" @on-change="pageChange" @on-page-size-change="PageSizeChange" show-sizer show-total></Page>
+            <Page :total="total" @on-change="pageChange" @on-page-size-change="PageSizeChange" show-sizer show-total></Page>
             </div>
         </TabPane>
         <TabPane label="信贷机构" >
@@ -153,16 +156,10 @@ export default {
           label: '手机号'
         }
       ],
-      cityType: [
-        {
-          value: '上海',
-          label: '上海'
-        },
-        {
-          value: '北京',
-          label: '北京'
-        }
-      ],
+      cityType: [],
+      cityTypel: [],
+      cityType1: [],
+      cityType2:[],
       status: [
         {
           value: '已咨询',
@@ -421,17 +418,17 @@ export default {
       columns10: [
         {
           title: 'ID',
-          key: 'id',
+          key: 'dataId',
           align: 'center'
         },
         {
           title: '手机号',
-          key: 'phone',
+          key: 'phoneMember',
           align: 'center'
         },
         {
           title: '姓名',
-          key: 'name',
+          key: 'realName',
           align: 'center'
         },
         {
@@ -441,32 +438,32 @@ export default {
         },
         {
           title: '所属区域',
-          key: 'region',
+          key: 'loanLocationName',
           align: 'center'
         },
         {
           title: '状态',
-          key: 'type',
+          key: 'servicePutawayStatus',
           align: 'center'
         },
         {
-          title: '虚拟余额',
-          key: 'fictitious',
+          title: '服务介绍初始状态',
+          key: 'serviceIntroductionStatus',
           align: 'center'
         },
         {
-          title: '现金余额(元)',
-          key: 'cash',
+          title: '已邀请人数',
+          key: 'loanInviterCodeCount',
           align: 'center'
         },
         {
           title: '注册时间',
-          key: 'register',
+          key: 'registerTime',
           align: 'center'
         },
         {
-          title: '最后登录时间',
-          key: 'sign',
+          title: '最后一次登录时间',
+          key: 'lastLoginTime',
           align: 'center'
         },
         {
@@ -498,20 +495,7 @@ export default {
           }
         }
       ],
-      data9: [
-        {
-          id: 'A11111',
-          phone: '135****7766',
-          name: '唐哈哈',
-          gender: '女',
-          region: '上海',
-          type: '上架',
-          fictitious: '2',
-          cash: '32',
-          register: '2018-03-29 15:12:34',
-          sign: '2018-03-29 15:12:34'
-        }
-      ],
+      data9: [],
       columns11: [
         {
           title: 'ID',
@@ -605,7 +589,11 @@ export default {
           number: '11',
           time: '2018-03-29 15:12:34'
         }
-      ]
+      ],
+      startRow: 1,
+      endRow: 10,
+      total: 0,
+      modell : ''
     }
   },
   methods: {
@@ -621,18 +609,110 @@ export default {
       this.data6.splice(index, 1)
     },
     pageChange (page) {
+      this.startRow = page
+      this.labell1(3)
       this.params.page = page
     },
     PageSizeChange (limit) {
+      this.endRow = limit
+      console.log(limit)
       this.params.limit = limit
     },
     addManage () {
       this.$router.push({ path: './addMechanism' })
     },
-    // 入驻待审核
-    labelll() {
-      console.log(111)
+    post(httpUrl,params,num) {
+      this.http.post(httpUrl,params).then(data=>{
+        console.log(num)
+        console.log(data)
+        if(data.code == 'success'){
+          if(num == 3){
+            this.data9 = data.data.dataList
+            this.total = parseInt(data.data.total) 
+          }
+        }
+      }).catch(err=>{
+        console.log(err)
+      })
+    },
+    //tab 栏
+    labell1(name) {
+      let parameter = {
+          pageSize : this.endRow,
+          pageNum : this.startRow,
+      }
+      let data
+      //入驻待审核
+      if(name == 0){
+        data = Object.assign({
+
+        },parameter)
+        this.post(BASE_URL + '/loan/officer/queryOfficerAdmissionList',data,0)
+      }
+      //资料改带审核
+      if(name == 1){
+        data = Object.assign({
+
+        },parameter)
+        this.post(BASE_URL + '/loan/officer/queryOfficerDataWaitCheckList',data,1)
+      }
+      //注册无资料
+      if(name == 2){
+        data = Object.assign({
+
+        },parameter)
+        this.post(BASE_URL + '/loan/officer/queryOfficerNoneDataList',data,2)
+      }
+      //信贷员列表
+      if(name == 3){
+        data = Object.assign({
+          queryStr : '',
+          searchOptions : '',
+          servicePutawayStatus : null,
+          loanAdCodeFirst : '', //省编码
+          loanAdCodeSecond : '', //市编码
+          serviceIntroductionStatus : null //服务介绍状态
+        },parameter)
+        console.log(data)
+        this.post(BASE_URL + '/loan/officer/queryOfficerManagerList',data,3)
+        return false
+      }
+      //信贷机构
+      if(name == 4){
+        data = Object.assign({
+
+        },parameter)
+        this.post(BASE_URL + '/loan/creditInstitutions/queryCreditInstitutionsList',data,4)
+      }
+      
+    },
+    //省 
+    citys(v){
+      console.log(v)
+      let data = {
+        "data": {
+          "data": v
+        }
+      }
+      this.httpshi(data)
+    },
+    //市
+    cityh(v){
+      console.log(v)
+    },
+    // 市接口
+    httpshi(data) {
+      this.http.post(BASE_URL+"/loan/hotcity/queryCityAddressList",data).then(data=>{
+        console.log(data)
+      })
     }
+  },
+  created() {
+    this.http.get('../../../static/city.json').then(data=>{
+      this.cityType = data
+      this.cityType1 = data
+      this.cityType2 = data
+    })
   }
 }
 </script>
