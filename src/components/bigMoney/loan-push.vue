9b2c22d3bba3df23389413fa70b82eb8d46b3fe1
齐玉飞 <qiyufei@netwisdom.cn>
2018-07-30 21:58:48 +0800

    大网钱优化

--- src/components/bigMoney/loan-push.vue
+++ src/components/bigMoney/loan-push.vue
@@ -279,6 +279,7 @@ export default {
         {
           title: '推送状态',
           align: 'center',
+          width: 100,
           render: (h, params) => {
             let pushStatus
             if (params.row.pushStatus == '0') {
@@ -296,12 +297,13 @@ export default {
         {
           title: '推送时间',
           align: 'center',
+          width: 160,
           key: 'pushTime'
         },
         {
           title: '推送方式',
           align: 'center',
-          width: 150,
+          width: 100,
           render: (h, params) => {
             let Code
             if (params.row.origin == 0) {
@@ -317,6 +319,7 @@ export default {
          {
           title: '返回信息',
           align: 'center',
+          width: 100,
           render: (h, params) => {
             let code 
             if (!params.row.code) {
@@ -334,11 +337,13 @@ export default {
         {
           title: '城市',
           align: 'center',
+          width: 100,
           key: 'city'
         },
         {
           title: '打卡发薪',
           align: 'center',
+          width: 100,
           render: (h, params) => {
             let salaryGetForm 
             if (params.row.salaryGetForm == '0') {
@@ -354,6 +359,7 @@ export default {
         {
           title: '社保',
           align: 'center',
+          width: 80,
           render: (h, params) => {
             let socialSecurity 
             if (params.row.socialSecurity == '0') {
@@ -369,6 +375,7 @@ export default {
          {
           title: '公积金',
           align: 'center',
+          width: 80,
           render: (h, params) => {
             let accumulationFund 
             if (params.row.accumulationFund == '0') {
@@ -384,17 +391,36 @@ export default {
         {
           title: '渠道',
           align: 'center',
+          width: 100,
           key: 'channel'
         },
         {
           title: '创建时间',
           align: 'center',
+          width: 160,
           key: 'dataCreateTime'
         },
         {
           title: '错误信息',
           align: 'center',
-          key: 'errorMessage'
+          width: 150,
+          render: (h, params) => {
+						return h('div', [
+						h('span', {
+							style: {
+							display: 'inline-block',
+							width: '100%',
+							overflow: 'hidden',
+							textOverflow: 'ellipsis',
+							whiteSpace: 'nowrap'
+							},
+							domProps: {
+							title: params.row.errorMessage
+							}
+						}, params.row.errorMessage)
+						])
+						}
+
         },
       ],
       // 秒贷
@@ -402,16 +428,19 @@ export default {
         {
           title: '姓名',
           align: 'center',
+          width: 100,
           key: 'name'
         },
         {
           title: '手机号',
           align: 'center',
+          width: 110,
           key: 'cellphone'
         },
          {
           title: '推送状态',
           align: 'center',
+          width: 100,
           render: (h, params) => {
             let pushStatus
             if (params.row.pushStatus == '0') {
@@ -429,12 +458,13 @@ export default {
          {
           title: '推送时间',
           align: 'center',
+          width: 160,
           key: 'pushTime'
         },
         {
           title: '推送方式',
           align: 'center',
-          width: 150,
+          width: 100,
           render: (h, params) => {
             let Code
             if (params.row.origin == 0) {
@@ -450,6 +480,7 @@ export default {
         {
           title: '返回状态',
           align: 'center',
+          width: 100,
           render: (h, params) => {
             let code
             if (params.row.code == '-1') {
@@ -467,27 +498,47 @@ export default {
         {
           title: '返回信息',
           align: 'center',
-          key: 'msg'
+          width: 160,
+          render: (h, params) => {
+						return h('div', [
+						h('span', {
+							style: {
+							display: 'inline-block',
+							width: '100%',
+							overflow: 'hidden',
+							textOverflow: 'ellipsis',
+							whiteSpace: 'nowrap'
+							},
+							domProps: {
+							title: params.row.msg
+							}
+						}, params.row.msg)
+						])
+						}
         },
         {
           title: '城市编码',
           align: 'center',
+          width: 120,
           key: 'belongCity'
         },
         {
           title: '媒体来源编号',
           align: 'center',
+          width: 120,
           key: 'sourceId'
         },
         {
           title: '创建时间',
           align: 'center',
+          width: 160,
           key: 'dataCreateTime'
         },
 
         {
           title: '验证结果',
           align: 'center',
+          width: 100,
           render: (h, params) => {
             let validResult
             if (params.row.validResult == 'success') {
@@ -505,6 +556,7 @@ export default {
         {
           title: '数据有效性',
           align: 'center',
+          width: 100,
           key: 'validFailMsg'
         },
       ],
@@ -513,16 +565,19 @@ export default {
         {
           title: '姓名',
           align: 'center',
+          width: 100,
           key: 'name'
         },
         {
           title: '手机号',
           align: 'center',
+          width: 110,
           key: 'phone'
         },
         {
           title: '推送状态',
           align: 'center',
+          width: 100,
           render: (h, params) => {
             let pushStatus
             if (params.row.pushStatus == '0') {
@@ -540,12 +595,13 @@ export default {
         {
           title: '推送时间',
           align: 'center',
+          width: 160,
           key: 'pushTime'
         },
         {
           title: '推送方式',
           align: 'center',
-          width: 150,
+          width: 100,
           render: (h, params) => {
             let Code
             if (params.row.origin == 0) {
@@ -561,6 +617,7 @@ export default {
         {
           title: '返回状态',
           align: 'center',
+          width: 100,
           render: (h, params) => {
             let returnCode
             if (params.row.returnCode == '0000') {
@@ -578,37 +635,59 @@ export default {
         {
           title: '返回信息',
           align: 'center',
-          key: 'returnMsg'
+          width: 150,
+          render: (h, params) => {
+						return h('div', [
+						h('span', {
+							style: {
+							display: 'inline-block',
+							width: '100%',
+							overflow: 'hidden',
+							textOverflow: 'ellipsis',
+							whiteSpace: 'nowrap'
+							},
+							domProps: {
+							title: params.row.returnMsg
+							}
+						}, params.row.returnMsg)
+						])
+						}
         },
         {
           title: '供应商',
           align: 'center',
+          width: 100,
           key: 'sid'
         },
         {
           title: '渠道',
           align: 'center',
+          width: 100,
           key: 'cid'
         },
         {
           title: '广告位ID',
           align: 'center',
+          width: 160,
           key: 'supplierCodeSub'
         },
        
         {
           title: '城市编码',
           align: 'center',
+          width: 100,
           key: 'cityCode'
         },
         {
           title: '生日',
           align: 'center',
+          width: 150,
           key: 'birthday'
         },
         {
           title: '创建时间',
           align: 'center',
+          width: 150,
           key: 'dataCreateTime'
         },
       ],
@@ -1496,7 +1575,22 @@ export default {
           title: '错误信息',
           align: 'center',
           width: 150,
-          key: 'errorMsg'
+          render: (h, params) => {
+						return h('div', [
+						h('span', {
+							style: {
+							display: 'inline-block',
+							width: '100%',
+							overflow: 'hidden',
+							textOverflow: 'ellipsis',
+							whiteSpace: 'nowrap'
+							},
+							domProps: {
+							title: params.row.errorMsg
+							}
+						}, params.row.errorMsg)
+						])
+						}
         },
       ],
       data1: []
