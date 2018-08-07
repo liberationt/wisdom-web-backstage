62aed00ad4b9e0bd2d39797b479f71fdb1347f10
齐玉飞 <qiyufei@netwisdom.cn>
2018-08-07 11:27:46 +0800

    会员列表

--- src/components/member-management/memberList.vue
+++ src/components/member-management/memberList.vue
@@ -7,27 +7,44 @@
     </div>
     <div class="clearfix">
       <div class="left">
-      <Select v-model="model1" placeholder="姓名" style="width:100px">
-        <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
-      </Select>
-      <Input v-model="name" placeholder="请输入关键字" style="width: 150px"></Input>
-      <Select v-model="model2" placeholder="所有渠道" style="width:150px;margin-left:20px">
+        <ul class="querysty">
+          <li>
+            <Select v-model="model1" placeholder="姓名" style="width:100px">
+              <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+            </Select>
+            <Input v-model="name" placeholder="请输入关键字" style="width: 150px;margin-left:-3px"></Input>
+          </li>
+          <li>
+            <span class="ml20">实名状态:</span>
+            <Select v-model="model3" placeholder="实名状态" style="width:150px;">
+              <Option v-for="item in reaName" :value="item.value" :key="item.value">{{ item.label }}</Option>
+            </Select>
+          </li>
+          <li>
+            <span class="ml20">账户状态:</span>
+            <Select v-model="model4" placeholder="账户状态" style="width:150px;">
+              <Option v-for="item in account" :value="item.value" :key="item.value">{{ item.label }}</Option>
+            </Select>
+          </li>
+          <li>
+            <span class="ml20">排序:</span>
+            <Select v-model="model5" placeholder="注册时间排序" style="width:150px;">
+              <Option v-for="item in registerTime" :value="item.value" :key="item.value">{{ item.label }}</Option>
+            </Select>
+          </li>
+        </ul>     
+      <!-- <Select v-model="model2" placeholder="所有渠道" style="width:150px;margin-left:20px">
         <Option v-for="item in cityType" :value="item.value" :key="item.value">{{ item.label }}</Option>
-      </Select>
-      <Select v-model="model3" placeholder="实名状态" style="width:150px;margin-left:20px">
-        <Option v-for="item in reaName" :value="item.value" :key="item.value">{{ item.label }}</Option>
-      </Select>
-      <Select v-model="model4" placeholder="账户状态" style="width:150px;margin-left:20px">
-        <Option v-for="item in account" :value="item.value" :key="item.value">{{ item.label }}</Option>
-      </Select>
-      <Select v-model="model5" placeholder="注册时间排序" style="width:150px;margin-left:20px">
-        <Option v-for="item in registerTime" :value="item.value" :key="item.value">{{ item.label }}</Option>
-      </Select>
+      </Select> --> 
       </div>
-      <Button class="right mr100" type="primary" icon="ios-search">查询</Button>
+      <Button type="info" class=" ml50 w100" :loading="loading3" @click="inquire">
+        <span v-if="!loading3">查询</span>
+        <span v-else>查询</span>
+      </Button>
+      <!-- <Button class="right mr100" type="primary" icon="ios-search">查询</Button> -->
     </div>
     <div id="application_table">
-      <Table border :columns="columns7" :data="data6"></Table>
+      <Table border highlight-row :columns="columns7" :data="data6"></Table>
     </div>
     <div class="tr mt15">
       <Page :total="100" @on-change="pageChange" @on-page-size-change="PageSizeChange" show-elevator show-sizer show-total></Page>
@@ -38,16 +55,8 @@
 export default {
   data () {
     return {
-      cityList: [
-        {
-          value: '姓名',
-          label: '姓名'
-        },
-        {
-          value: '手机号',
-          label: '手机号'
-        }
-      ],
+      loading3: false,
+      cityList: [],
       cityType: [
         {
           value: '官方APP',
@@ -70,40 +79,9 @@ export default {
           label: '渠道2'
         }
       ],
-      reaName: [
-        {
-          value: '未实名',
-          label: '未实名'
-        },
-        {
-          value: '已实名',
-          label: '已实名'
-        }
-      ],
-      account: [
-        {
-          value: '正常',
-          label: '正常'
-        },
-        {
-          value: '冻结',
-          label: '冻结'
-        }
-      ],
-      registerTime: [
-        {
-          value: '登陆时间排序',
-          label: '登陆时间排序'
-        },
-        {
-          value: '金币数量排序',
-          label: '金币数量排序'
-        },
-        {
-          value: '余额大小排序',
-          label: '余额大小排序'
-        }
-      ],
+      reaName: [],
+      account: [],
+      registerTime: [],
       model1: '',
       model2: '',
       model3: '',
@@ -131,17 +109,17 @@ export default {
           align: 'center'
         },
         {
-          title: '余额',
-          key: 'actualtime',
-          align: 'center'
-        },
-        {
           title: '注册渠道',
           key: 'platform',
           align: 'center'
         },
         {
           title: '账户状态',
+          key: 'object',
+          align: 'center'
+        },
+        {
+          title: '已邀请人数',
           key: 'object',
           align: 'center'
         },
@@ -331,6 +309,20 @@ export default {
     PageSizeChange (limit) {
       this.params.limit = limit
     }
+  },
+  mounted () {
+    this.http.post(BASE_URL + '/loan/userInfo/queryUserMemberListFilter', {})
+    .then((resp) => {
+      if (resp.code == 'success') {
+        this.cityList = resp.data.searchOptions
+        this.reaName = resp.data.realNameStatusOptions
+        this.account = resp.data.accountStatusOptions
+        this.registerTime = resp.data.userTimeOptions
+      } else {
+      }
+    })
+    .catch(() => {
+    })
   }
 }
 </script>
