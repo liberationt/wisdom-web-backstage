e7673e19002975e64a4194f8d11f2f39cfbb9f96
localbiz <localbiz@netwisdom.cn>
2018-08-15 09:43:15 +0800

    还原配置

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,7 +5,7 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   // API_HOST: '"http://dev.busadapter.zanfin.com"' // 开发环境接口地址(这里是增加的内容)
-  API_HOST: '"http://192.168.1.85:3330"', // 开发环境接口地址(这里是增加的内容)
+  API_HOST: '"http://dev.busadapter.zanfin.com"', // 开发环境接口地址(这里是增加的内容)
 })
 
 // http://dev.busadapter.zanfin.com
