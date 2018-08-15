f9fb0de8a39e1301dc38f5dad3000a4009f55ea6
liulili <liulili@netwisdom.cn>
2018-08-15 20:26:46 +0800

    版本页面

--- src/components/basic-configuration/broadcast-bigimg.vue
+++ src/components/basic-configuration/broadcast-bigimg.vue
@@ -10,39 +10,52 @@
             轮播大图 - 首页
             <p class="homePage_button right" @click="homePageAdd"><Icon type="android-add"></Icon>添加</p>
         </h1>
-        <div class="homePage_bianli"  v-for="item in items"   >
+        <div class="homePage_bianli"  v-for="(item,index) in items" >
             <div class="homePage_img clearfix">
-                <span class="homePage_img_img left">
-                    <img src="../../image/dkcslogo.jpg" alt="">
-                </span>
+                <Upload
+                    :format="['jpg','jpeg','png']"
+                    :on-format-error="handleFormatError1"
+                    :before-upload="handleUpload"
+                    :data="{'uptoken': uptoken}"
+                    :show-upload-list="false"                        
+                    action=''
+                    class="homePage_img_img left">
+                    <div >
+                      <span class="homePage_img_img left">
+                          <img :src="item.banklogo" alt="">
+                      </span>
+                    </div>
+                </Upload>
+                
                 <span class="homePage_img_input left">
-                    <Select v-model="model1" style="width:100px" @on-change="homePage1">
-                      <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+                    <Select v-model="item.model1" style="width:100px">
+                      <Option v-for="item in item.cityList" @click.native="homePage1(index,item.value)" :value="item.value" :key="item.value">{{ item.label }}</Option>
                     </Select>
-                    <input type="text" class="homePage_url" v-if="homePage_if_input">
+                    <input type="text" class="homePage_url" v-if="item.homePage_if_input">
                 </span>
-                <span class="homePage_img_input left"  v-if="homePage_if">
-                    <Select v-model="model2" style="width:100px" @on-change="homePage2">
-                      <Option v-for="item in cityList2" :value="item.value" :key="item.value">{{ item.label }}</Option>
+                <span class="homePage_img_input left"  v-if="item.homePage_if">
+                    <Select v-model="item.model2" style="width:100px"  >
+                      <Option v-for="item in item.cityList2" @click.native="homePage2(index,item.value)" :value="item.value" :key="item.value">{{ item.label }}</Option>
                     </Select>
                 </span>
-                <span class="homePage_img_input left"  v-if="homePage_if1">
-                    <Select v-model="model3" style="width:100px">
-                      <Option v-for="item in cityList3" :value="item.value" :key="item.value">{{ item.label }}</Option>
+                <span class="homePage_img_input left"  v-if="item.homePage_if1">
+                    <Select v-model="item.model3" style="width:100px">
+                      <Option v-for="item in item.cityList3" :value="item.value" :key="item.value">{{ item.label }}</Option>
                     </Select>
                 </span>
                 <div class="homePage_input2 left">
                   <p>
-                      <Select v-model="model4" style="width:120px">
-                          <Option v-for="item in cityList4" :value="item.value" :key="item.value">{{ item.label }}</Option>
+                      <Select v-model="item.model4" style="width:120px">
+                          <Option v-for="item in item.cityList4" :value="item.value" :key="item.value">{{ item.label }}</Option>
                       </Select>
                   </p>
                   <p>
-                      <Select v-model="model5" style="width:120px">
-                          <Option v-for="item in cityList5" :value="item.value" :key="item.value">{{ item.label }}</Option>
+                      <Select v-model="item.model5" style="width:120px">
+                          <Option v-for="item in item.cityList5" :value="item.value" :key="item.value">{{ item.label }}</Option>
                       </Select>
                   </p>
                 </div>
+                <span class="homePage_delete right"><a href="javascript:;" class="version_icon">保存</a> </span>
                 <span class="homePage_delete right">
                     <span class="version_icon">
                       <Poptip
@@ -55,7 +68,7 @@
                       </Poptip>
                     </span>
                 </span>
-                <span class="homePage_delete right"><a href="javascript:;" class="version_icon">保存</a> </span>
+                <span class="homePage_delete1 right"><InputNumber class="banknumint" :min="0" v-model="item.bankNo"></InputNumber> </span>
             </div>
         </div>
         <hr>
@@ -68,137 +81,257 @@
 </template>
 <script>
 export default {
-  data () {
+  data() {
     return {
-      cityList: [
-        {
-          value: '不跳转',
-          label: '不跳转'
-        },
-        {
-          value: '跳转',
-          label: '跳转'
-        }
-      ],
-      cityList2: [
-        {
-          value: '原生页面',
-          label: '原生页面'
-        },
-        {
-          value: 'h5页面',
-          label: 'h5页面'
-        }
-      ],
-      cityList3: [
-        {
-          value: '首页',
-          label: '首页'
-        },
-        {
-          value: '贷款列表',
-          label: '贷款列表'
-        },
-        {
-          value: '产品详情',
-          label: '产品详情'
-        },
-        {
-          value: '实名认证',
-          label: '实名认证'
-        },
-        {
-          value: '精准推荐',
-          label: '精准推荐'
-        },
-        {
-          value: '我要赚钱',
-          label: '我要赚钱'
-        },
-        {
-          value: '赚了花',
-          label: '赚了花'
-        }
-      ],
-      cityList4: [
-        {
-          value: '下架',
-          label: '下架'
-        },
-        {
-          value: '上架',
-          label: '上架'
-        }
-      ],
-      cityList5: [
-        {
-          value: '未实名显示',
-          label: '未实名显示'
-        },
-        {
-          value: '已实名隐藏',
-          label: '已实名隐藏'
-        }
-      ],
-      model1: '不跳转',
-      model2: '原生页面',
-      model3: '首页',
-      model4: '下架',
-      model5: '未实名显示',
-      homePage_if_input: false,
-      homePage_if: false,
-      homePage_if1: false,
       items: [
         {
-          url: 'http:baidu.com'
+          cityList: [
+            {
+              value: "不跳转",
+              label: "不跳转"
+            },
+            {
+              value: "跳转",
+              label: "跳转"
+            }
+          ],
+          cityList2: [
+            {
+              value: "原生页面",
+              label: "原生页面"
+            },
+            {
+              value: "h5页面",
+              label: "h5页面"
+            }
+          ],
+          cityList3: [
+            {
+              value: "首页",
+              label: "首页"
+            },
+            {
+              value: "贷款列表",
+              label: "贷款列表"
+            },
+            {
+              value: "产品详情",
+              label: "产品详情"
+            },
+            {
+              value: "实名认证",
+              label: "实名认证"
+            },
+            {
+              value: "精准推荐",
+              label: "精准推荐"
+            },
+            {
+              value: "我要赚钱",
+              label: "我要赚钱"
+            },
+            {
+              value: "赚了花",
+              label: "赚了花"
+            }
+          ],
+          cityList4: [
+            {
+              value: "下架",
+              label: "下架"
+            },
+            {
+              value: "上架",
+              label: "上架"
+            }
+          ],
+          cityList5: [
+            {
+              value: "未实名显示",
+              label: "未实名显示"
+            },
+            {
+              value: "已实名隐藏",
+              label: "已实名隐藏"
+            }
+          ],
+          model1: "不跳转",
+          model2: "原生页面",
+          model3: "首页",
+          model4: "下架",
+          model5: "未实名显示",
+          homePage_if_input: false,
+          homePage_if: false,
+          homePage_if1: false,
+          banklogo: require('../../image/moren.png'),
         }
       ]
-    }
+    };
   },
   components: {},
   methods: {
-    ok () {
-      this.$Message.info('删除成功！')
+    ok() {
+      this.$Message.info("删除成功！");
     },
-    cancel () {
-      this.$Message.info('删除失败！')
+    cancel() {
+      this.$Message.info("删除失败！");
     },
-    homePage1 () {
-      if (this.model1 === '不跳转') {
-        this.homePage_if = false
-        this.homePage_if1 = false
-        this.homePage_if_input = false
-      } else if (this.model1 === '跳转') {
-        this.homePage_if = true
-        this.homePage_if1 = true
-        if (this.model2 === 'h5页面') {
-          this.homePage_if1 = false
-          this.homePage_if_input = true
-        };
+    homePage1(v,t) {
+      console.log(t)
+      if (t == "不跳转") {
+        this.items[v].homePage_if = false;
+        this.items[v].homePage_if1 = false;
+        this.items[v].homePage_if_input = false;
+      } else if (t == "跳转") {
+        this.items[v].homePage_if = true;
+        this.items[v].homePage_if1 = true;
+        if (t == "h5页面") {
+          this.items[v].homePage_if1 = false;
+          this.items[v].homePage_if_input = true;
+        }
       }
     },
-    homePage2 () {
-      if (this.model2 === '原生页面') {
-        this.homePage_if1 = true
-        this.homePage_if_input = false
+    homePage2(v,t) {
+      if (t == "原生页面") {
+        this.items[v].homePage_if1 = true;
+        this.items[v].homePage_if_input = false;
       } else {
-        this.homePage_if1 = false
-        this.homePage_if_input = true
+        this.items[v].homePage_if1 = false;
+        this.items[v].homePage_if_input = true;
       }
     },
-    homePageAdd () {
-      this.index++
-      this.items.push({
-        url: 'http:baidu.com'
-      })
-    }
+    homePageAdd() {
+      this.index++;
+      this.items.push( {
+          cityList: [
+            {
+              value: "不跳转",
+              label: "不跳转"
+            },
+            {
+              value: "跳转",
+              label: "跳转"
+            }
+          ],
+          cityList2: [
+            {
+              value: "原生页面",
+              label: "原生页面"
+            },
+            {
+              value: "h5页面",
+              label: "h5页面"
+            }
+          ],
+          cityList3: [
+            {
+              value: "首页",
+              label: "首页"
+            },
+            {
+              value: "贷款列表",
+              label: "贷款列表"
+            },
+            {
+              value: "产品详情",
+              label: "产品详情"
+            },
+            {
+              value: "实名认证",
+              label: "实名认证"
+            },
+            {
+              value: "精准推荐",
+              label: "精准推荐"
+            },
+            {
+              value: "我要赚钱",
+              label: "我要赚钱"
+            },
+            {
+              value: "赚了花",
+              label: "赚了花"
+            }
+          ],
+          cityList4: [
+            {
+              value: "下架",
+              label: "下架"
+            },
+            {
+              value: "上架",
+              label: "上架"
+            }
+          ],
+          cityList5: [
+            {
+              value: "未实名显示",
+              label: "未实名显示"
+            },
+            {
+              value: "已实名隐藏",
+              label: "已实名隐藏"
+            }
+          ],
+          model1: "不跳转",
+          model2: "原生页面",
+          model3: "首页",
+          model4: "下架",
+          model5: "未实名显示",
+          banklogo: require('../../image/moren.png'),
+        });
+    },
+    handleFormatError1(file) {
+      // this.formCustom.productlogo = ''
+      this.$Message.info("图片格式不正确,请上传正确的图片格式");
+    },
+    handleUpload(file) {
+      console.log(file)
+      let splic = file.name.split(".");
+      if (
+        splic[splic.length - 1] == "png" ||
+        splic[splic.length - 1] == "jpg" ||
+        splic[splic.length - 1] == "gif" ||
+        splic[splic.length - 1] == "jpeg"
+      ) {
+        let formData = new FormData();
+        formData.append("file", file);
+        let config = {
+          headers: {
+            "Content-Type": "multipart/form-data"
+          },
+          timeout: 1000 * 60 * 5
+        };
+        this.http
+          .post(BASE_URL + "/fileUpload", formData, config)
+          .then(resp => {
+            if (resp.code == "success") {
+              // this.formValidate.productlogo = resp.data;
+              // this.formValidate.logoUrl1 = resp.data;
+            } else {
+            }
+          })
+          .catch(() => {});
+        // this.formValidate.productlogo = file.name;
+        // return false;
+      }
+      return false
+    },
   }
-}
+};
 </script>
 <style lang="less" scoped>
 .homePage_center {
   width: 100%;
   text-align: center;
+  
+}
+.uploadleft {
+  width: 58px;
+  height:58px;
+  position: relative;
+  left: 0px;
+  top: 0px;
 }
 .homePage_url {
   position: absolute;
@@ -208,13 +341,14 @@ export default {
   border: 1px solid #ccc;
   padding-left: 8px;
 }
-.homePage_submit , .homePage_goback{
-    display: inline-block;
-    height: 30px;
-    line-height: 30px;
-    text-align: center;
-    margin-top: 15px;
-    border-radius: 5px;
+.homePage_submit,
+.homePage_goback {
+  display: inline-block;
+  height: 30px;
+  line-height: 30px;
+  text-align: center;
+  margin-top: 15px;
+  border-radius: 5px;
 }
 .homePage_goback {
   background-color: #ccc;
@@ -222,41 +356,51 @@ export default {
   margin-left: 20px;
 }
 .homePage_submit {
-    background-color: #3598dc;
-    width: 80px;
-    color: #fff;
+  background-color: #3598dc;
+  width: 80px;
+  color: #fff;
 }
 hr {
-    margin-top: 25px;
+  margin-top: 25px;
 }
 .homePage_button {
-    width: 80px;
-    border-radius: 50px;
-    border: 1px solid #ccc;
-    background-color: #fff;
-    text-align: center;
-    cursor: pointer;
+  width: 80px;
+  border-radius: 50px;
+  border: 1px solid #ccc;
+  background-color: #fff;
+  text-align: center;
+  cursor: pointer;
 }
 .homePage_delete {
-    display: inline-block;
-    border: 1px solid #ccc;
-    height: 30px;
-    width: 80px;
-    text-align: center;
-    line-height: 30px;
-    border-radius: 50px;
-    margin-right: 10px;
-    margin-top: 40px;
-    cursor: pointer;
+  display: inline-block;
+  border: 1px solid #ccc;
+  height: 30px;
+  width: 80px;
+  text-align: center;
+  line-height: 30px;
+  border-radius: 50px;
+  margin-right: 10px;
+  margin-top: 40px;
+  cursor: pointer;
+}
+.homePage_delete1{
+  display: inline-block;
+  height: 30px;
+  width: 80px;
+  text-align: center;
+  line-height: 30px;
+  border-radius: 50px;
+  margin-right: 10px;
+  margin-top: 40px;
 }
 .homePage_input2 {
-    margin-left: 100px;
+  margin-left: 100px;
 }
-.homePage_input2 p{
-    margin-bottom: 25px;
+.homePage_input2 p {
+  margin-bottom: 25px;
 }
 .homePage_img_input {
-    margin-left: 40px;
+  margin-left: 40px;
 }
 .homePage_img_img {
   display: inline-block;
@@ -285,5 +429,5 @@ hr {
   margin-top: 20px;
   border-left: 5px solid #8bb4e7;
   position: relative;
-  }
+}
 </style>
