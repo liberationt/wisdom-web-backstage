02da34afc29fedf192277fed5f83cc468c58d8be
qiyufei <qiyufei@DESKTOP-JU351DF>
2018-09-07 10:29:25 +0800

    渠道

---
+++ src/components/unifiedChannel/channel-management.vue
@@ -0,0 +1,303 @@
+<template>
+  <div>
+    <div class="navigation">
+      <p>
+      <span>管理首页&nbsp;>&nbsp;系统&nbsp;>&nbsp;公共配置&nbsp;>&nbsp;黑名单管理</span>
+      </p>
+    </div>
+      <div class="clearfix conditioncss">
+        <div class="left">
+          <span>应用：</span>
+          <Select v-model="application" style="width:150px">
+            <Option v-for="item in cityList" :value="item.businessAlias" :key="item.businessAlias">{{ item.businessName }}</Option>
+          </Select>
+        </div>
+        <Button type="primary" @click="addchannel(1)" class="ml20">添加渠道</Button>
+        <Button type="primary" class="ml10">导出</Button>
+        <Button type="warning" class="ml10">批量更新</Button>
+        </div>
+        <div id="application_table" class="mt15 contentcss">
+          <Table border :columns="columns7" :data="data6"></Table>
+          <div class="tr mt15">
+            <Page :total="total" :page-size="endRow" @on-change="pageChange" @on-page-size-change="PageSizeChange" show-sizer show-total></Page>
+          </div>
+        </div>
+        <!-- 添加渠道 -->
+        <Modal
+          title="添加渠道"
+          v-model="modal10"
+           @on-ok="businessSubmit('formCustom')"
+           @on-cancel="businessReset('formCustom')"
+          ok-text="保存"
+          cancel-text="取消"
+          class-name="vertical-center-modal"
+          width="700"
+          :loading="loading"
+          :mask-closable="false">
+          <div  class="newtype_file mt15 mb15">
+            <Form ref="formCustom" :model="formCustombusi" :rules="ruleCustombusi" :label-width="100" style="padding-left:100px">
+                <FormItem v-if="numhid" label="渠道编号:" prop="channelnum" >
+                    <Input  v-model="formCustombusi.channelnum" placeholder="请输入渠道编号" disabled style="width: 300px"></Input>
+                </FormItem>
+                <FormItem label="渠道名称:" prop="channelname" >
+                    <Input  v-model="formCustombusi.channelname" placeholder="请输入渠道名称"  style="width: 300px"></Input>
+                </FormItem>
+                <FormItem label="基础折扣系数:" prop="coefficient" >
+                    <Input  v-model="formCustombusi.coefficient" placeholder="请输入基础折扣系数"  style="width: 300px">
+                        <span slot="append">%</span>
+                    </Input>
+                </FormItem>
+                <FormItem label="基础注册数:" prop="register" >
+                    <Input  v-model="formCustombusi.register" placeholder="请输入基础注册数"  style="width: 300px"></Input>
+                </FormItem>
+              <FormItem label="推广页样式:" prop="style" >
+              <Select v-model="formCustombusi.style" placeholder="请选择" style="width:300px" >
+                <Option value="1">华赞金服</Option>
+                <Option value="2">抢单侠</Option>
+              </Select>
+            </FormItem>
+            <FormItem label=""  >
+                <img @click="modal11=true" :src="stylelogo" alt="">
+            </FormItem>  
+          </Form>
+          </div>
+          </Modal>
+          <Modal v-model="modal11" fullscreen footer-hide>
+            <div class="imgenlarge">
+                <img :src="stylelogo" alt="" >
+            </div>
+          </Modal>
+        
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      city: "",
+      application: "",
+      cityList: [],
+      modal10:false,
+      modal11: false,
+      numhid:false,
+      stylelogo: require('../../image/moren.png'),
+      formCustombusi: {
+        channelnum: '',
+        channelname: '',  
+        coefficient: '',
+        register: '',
+        style: ''
+      },
+      ruleCustombusi: {
+        channelname: [
+          { required: true, message: '请输入渠道名称', trigger: 'blur' }
+        ],
+        coefficient: [
+          { required: true, message: '请输入基础折扣系数', trigger: 'blur' },
+        ],
+        register: [
+          { required: true, message: '请输入基础注册数', trigger: 'blur' }
+        ],
+        // phone: [
+        //   { required: true, message: '请输入手机号', trigger: 'blur' },
+        //   {required: true, message: '请输入正确的手机号', pattern: /^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\d{8}$/, trigger: 'blur'}
+        // ],
+        style: [
+          { required: true, message: '请选择推广页样式', trigger: 'blur' }
+        ]
+      },
+      columns7: [
+        {
+          title: "渠道编号",
+          key: "channelnum",
+          minWidth: 200,
+          align: "center"
+        },
+        {
+          title: "渠道名称",
+          key: "channelname",
+          minWidth: 200,
+          align: "center"
+        },
+        {
+          title: "基础折扣系数(%)",
+          key: "coefficient",
+          minWidth: 200,
+          align: "center",
+          render: (h, params) => {
+          return h('div', [
+            h('InputNumber', {
+              props: {
+                min:0,
+                value:params.row.numlots,
+              },
+              on:{
+                   input:val=>{
+                      params.row.numlots=val;
+                   }
+               }
+            },)
+          ]);
+        }
+        },
+        {
+          title: "基础注册数",
+          key: "registernum",
+          minWidth: 100,
+          align: "center",
+          render: (h, params) => {
+          return h('div', [
+            h('InputNumber', {
+              props: {
+                min:0,
+                value:params.row.numlots,
+              },
+              on:{
+                   input:val=>{
+                      params.row.numlots=val;
+                   }
+               }
+            },)
+          ]);
+        }
+        },
+        {
+          title: "推广URL",
+          key: "extensionurl",
+          minWidth: 100,
+          align: "center"
+        },
+        {
+          title: "操作",
+          key: "action",
+          minWidth: 150,
+          align: "center",
+          render: (h, params) => {
+            return h("div", [
+              h(
+                "Button",
+                {
+                  props: {
+                    type: "primary",
+                    size: "small"
+                  },
+                  style: {
+                    marginRight: "5px"
+                  },
+                  on: {
+                    click: () => {
+                        this.addchannel (2)
+                    }
+                  }
+                },
+                "编辑"
+              ),
+              h(
+                "Button",
+                {
+                  props: {
+                    type: "error",
+                    size: "small"
+                  },
+                  style: {
+                    marginRight: "5px"
+                  },
+                  on: {
+                    click: () => {
+                        this.channelremove ()
+                    }
+                  }
+                },
+                "删除"
+              )
+
+            ]);
+          }
+        }
+      ],
+      data6: [
+        {
+          channelnum:'LHHZ0002',
+          channelname: '渠道名称1',
+          extensionurl: 'http://m.zanfin.com/reg.html?qd=LHHZ002'
+        }        
+      ],
+      cityTypel: [],
+      startRow: 1,
+      endRow: 20,
+      startRow1: 1,
+      endRow1: 20,
+      total: 0,
+      loading3: false
+    };
+  },
+  methods: {
+    messagetype() {},
+    // // 待审核
+    pageChange(page) {
+      this.startRow = page;
+      this.params.page = page;
+      this.labell1();
+    },
+    PageSizeChange(limit) {
+      this.startRow = 1;
+      this.endRow = limit;
+      this.params.limit = limit;
+    },
+    businessSubmit (name) {
+      this.$refs[name].validate((valid) => {
+        if (!valid) {
+          return this.changeLoading()
+        } else {     
+          this.$Message.error('Success!')
+        }
+      })
+    },
+    businessReset (name) {
+      this.$refs[name].resetFields()
+    },
+    // 添加渠道 
+    addchannel (num) {
+        if (num == 2) {
+            this.numhid = true
+        }  else {
+            this.numhid = false
+        }
+        this.modal10 = true
+    },
+    // 删除
+    channelremove () {
+       this.$Modal.confirm({
+            title: '提示',
+            content: '<p>确定要删除吗?</p>',
+            onOk: () => {
+                this.$Message.info('Clicked ok');
+            }
+        }); 
+    }
+
+
+  },
+  created() {
+    this.application = "56DECD723B2151ECE39F98693F904E3E"
+    this.http.post(BASE_URL + "/black/riskBlackList/geBusinessAliastList ",{}).then(data=>{
+      if(data.code == 'success'){
+        this.cityList = data.data
+      }
+    }).catch(err=>{
+      console.log(err)
+    })
+
+  }
+};
+</script>
+<style lang="less" scoped>
+.imgenlarge{
+    padding: 50px;
+    img{
+        width: 100%;
+        height: 100%;
+
+    }
+}
+</style>
---
+++ src/components/unifiedChannel/supplier-list.vue
@@ -0,0 +1,485 @@
+<template>
+  <div>
+    <div class="navigation">
+      <p>
+      <span>管理首页&nbsp;>&nbsp;渠道&nbsp;>&nbsp;渠道配置</span>
+      </p>
+    </div>
+      <div class="clearfix conditioncss">
+        <div class="left">
+          <span>供应商名称:</span>
+          <Input v-model="number" class="ml5" placeholder="请输入供应商名称" style="width: 150px"></Input>
+        </div>
+        <Button type="info" class="left ml20 w90" :loading="loading3" @click="label_query('warning')">
+          <span v-if="!loading3">查询</span>
+          <span v-else>查询</span>
+        </Button>
+          <Button shape="circle" class="left ml20" @click="addsupplier">添加供应商</Button>
+        
+        </div>
+        <div id="application_table" class="mt15 contentcss">
+            <ul>
+                <li>
+                    <div class="titletab clearfix" >
+                    <div class="left">
+                        <strong>S0003</strong>
+                        <strong class="ml10">供应商名称</strong>
+                        <span class="enterprise ml10">企业</span>
+                        <span class="ml10 accountnum">账号正常</span>
+                    </div>
+                    <div class="right">
+                        <Button type="primary" @click="tabshow(0)">查看供应商</Button>
+                        <Button type="info"  @click="addbusiness">添加</Button>
+                        <Button type="success" @click="addsupplier">编辑</Button>
+                        <Button type="error" @click="suppremove">删除</Button>
+                    </div>                
+                </div>
+                <Table class="hidden tabhid" border :columns="columns7" :data="data6"></Table>
+                </li>
+                <li>
+                    <div class="titletab clearfix" >
+                    <div class="left">
+                        <strong>S0003</strong>
+                        <strong class="ml10">供应商名称</strong>
+                        <span class="enterprise ml10">企业</span>
+                        <span class="ml10 accountnum">账号正常</span>
+                    </div>
+                    <div class="right">
+                        <Button type="primary" @click="tabshow(1)">查看供应商</Button>
+                        <Button type="info">添加</Button>
+                        <Button type="success">编辑</Button>
+                        <Button type="error">删除</Button>
+                    </div>                
+                </div>
+                <Table class="hidden tabhid" border :columns="columns7" :data="data6"></Table>
+                </li>
+            </ul>          
+          <div class="tr mt15">
+            <Page :total="total" :page-size="endRow" @on-change="pageChange" @on-page-size-change="PageSizeChange" show-sizer show-total></Page>
+          </div>
+        </div>
+        <!-- 添加编辑供应商弹框 -->
+        <Modal
+          title="添加供应商"
+          v-model="modal9"
+           @on-ok="handleSubmit('formCustom')"
+           @on-cancel="handleReset1('formCustom')"
+          ok-text="保存"
+          cancel-text="取消"
+          class-name="vertical-center-modal"
+          width="800"
+          :loading="loading"
+          :mask-closable="false">
+          <div  class="newtype_file mt15 mb15">
+            <Form ref="formCustom" :model="formCustom" :rules="ruleCustom" :label-width="100" style="padding-left:200px">
+              <FormItem label="供应商编号:" prop="channelnum" >
+                <Input v-model="formCustom.channelnum" placeholder="请输入供应商编号" style="width: 300px"></Input>
+              </FormItem>
+              <FormItem label="类型:" prop="channelid" >
+                <Select v-model="formCustom.channeltype" placeholder="请选择" style="width:100px">
+                <Option value="1">企业</Option>
+                <Option value="2">个人</Option>
+              </Select>
+                <Input v-model="formCustom.channelid" placeholder="请输入证件号码" style="width: 200px"></Input>
+              </FormItem>
+              <FormItem label="供应商名称:" prop="suppname" >
+                <Input v-model="formCustom.suppname"  placeholder="请输入供应商名称" style="width: 300px"></Input>
+              </FormItem>
+            <FormItem label="联系人:" class="clearfix contacts" >
+              <Row>
+                <Col span="8">
+                  <FormItem prop="name" class="nametop">
+                    <Input class="left" v-model="formCustom.name" placeholder="请输入姓名" style="width: 150px">
+                      <span slot="prepend">姓名</span>
+                    </Input>
+                  </FormItem>
+                </Col>
+                <Col span="7">
+                  <FormItem prop="phone" class="nametop">                     
+                    <Input v-model="formCustom.phone" class="left " placeholder="请输入手机号" style="width: 150px;margin-top:1px">
+                      <span slot="prepend">手机号</span>
+                    </Input>
+                  </FormItem>
+                </Col>
+              </Row> 
+            </FormItem>
+            <FormItem label="备注:" prop="remarks" >
+              <Input  v-model="formCustom.remarks" placeholder="请输入备注" style="width: 300px"></Input>
+            </FormItem>
+            <FormItem label="账户状态:" prop="accounttype" >
+              <Select v-model="formCustom.accounttype" placeholder="请选择" style="width:300px" >
+                <Option value="1">正常</Option>
+                <Option value="2">暂停</Option>
+              </Select>
+            </FormItem> 
+            <FormItem label="登录密码:"  >
+              <Button type="primary">自动重置密码</Button>
+            </FormItem>     
+          </Form>
+          </div>
+          </Modal>
+          <!-- 添加编辑业务 -->
+          <!-- 添加编辑供应商弹框 -->
+        <Modal
+          title="添加业务-供应商名称"
+          v-model="modal10"
+           @on-ok="businessSubmit('formCustom')"
+           @on-cancel="businessReset('formCustom')"
+          ok-text="保存"
+          cancel-text="取消"
+          class-name="vertical-center-modal"
+          width="700"
+          :loading="loading"
+          :mask-closable="false">
+          <div  class="newtype_file mt15 mb15">
+            <Form ref="formCustom" :model="formCustombusi" :rules="ruleCustombusi" :label-width="100" style="padding-left:100px">
+              <FormItem label="推广业务:" prop="accounttype" >
+              <Select v-model="formCustom.accounttype" placeholder="请选择业务" style="width:300px" >
+                <Option value="1">华赞金服</Option>
+                <Option value="2">抢单侠</Option>
+              </Select>
+            </FormItem>
+            <FormItem label="商务负责人:" prop="person" >
+              <Select v-model="formCustom.person" placeholder="请选择商务对接" style="width:300px" >
+                <Option value="1">哈哈</Option>
+                <Option value="2">呵呵</Option>
+              </Select>
+            </FormItem>
+            <FormItem label="对方联系人:" class="clearfix contacts" >
+              <Row>
+                <Col span="8">
+                    <FormItem prop="name" class="nametop">
+                        <Input class="left" v-model="formCustom.name" placeholder="请输入姓名" style="width: 150px">
+                        <span slot="prepend">姓名</span>
+                        </Input>
+                    </FormItem>
+                </Col>
+                <Col span="7">
+                    <FormItem prop="phone" class="nametop">                     
+                      <Input v-model="formCustom.phone" class="left " placeholder="请输入手机号" style="width: 150px;margin-top:1px">
+                      <span slot="prepend">手机号</span>
+                      </Input>
+                    </FormItem>
+              </Col>
+            </Row> 
+            </FormItem>
+            <FormItem label="备注:" prop="remarks" >
+              <Input  v-model="formCustom.remarks" placeholder="请输入备注" style="width: 300px"></Input>
+            </FormItem>  
+          </Form>
+          </div>
+          </Modal>
+        
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      city: "",
+      modal9:false,
+      modal10:false,
+      formCustom: {
+        channelnum: '',
+        channeltype: '1',
+        channelid: '',
+        suppname:'',
+        name: '',
+        phone: '',
+        remarks: '',
+        accounttype: ''
+      },
+      ruleCustom: {
+        channelnum: [
+          { required: true, message: '请输入供应商编号', trigger: 'blur' },
+          { type: 'string', max: 10, message: '最多输入10个字符', trigger: 'blur' },
+          {required: true, message: '请输入正确的供应商编号', pattern: /^[a-zA-Z0-9]+$/, trigger: 'blur'}
+        ],
+        channelid: [
+          { required: true, message: '请输入证件号码', trigger: 'blur' },
+        ],
+        suppname: [
+          { required: true, message: '请输入供应商名称', trigger: 'blur' },
+          { type: 'string', max: 20, message: '最多输入20个字符', trigger: 'blur' },
+        ],
+        name: [
+          { required: true, message: '请输入姓名', trigger: 'blur' }
+        ],
+        phone: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          {required: true, message: '请输入正确的手机号', pattern: /^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\d{8}$/, trigger: 'blur'}
+        ],
+        remarks: [
+          { required: true, message: '请输入备注', trigger: 'blur' },
+          { type: 'string', max: 20, message: '最多输入20个字符', trigger: 'blur' },
+        ],
+        accounttype: { required: true, message: '请选择账户状态', trigger: 'blur' }
+      },
+      // 业务
+      formCustombusi: {
+        accounttype: '',
+        person: '',  
+        name: '',
+        phone: '',
+        remarks: ''
+      },
+      ruleCustombusi: {
+        accounttype: [
+          { required: true, message: '请选择推广业务', trigger: 'blur' }
+        ],
+        person: [
+          { required: true, message: '请选择商务负责人', trigger: 'blur' },
+        ],
+        name: [
+          { required: true, message: '请输入姓名', trigger: 'blur' }
+        ],
+        phone: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          {required: true, message: '请输入正确的手机号', pattern: /^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\d{8}$/, trigger: 'blur'}
+        ],
+        remarks: [
+          { required: true, message: '请输入备注', trigger: 'blur' },
+          { type: 'string', max: 20, message: '最多输入20个字符', trigger: 'blur' },
+        ]
+      },
+      columns7: [
+        {
+          title: "推广业务",
+          key: "blackType",
+          minWidth: 200,
+          align: "center"
+        },
+        {
+          title: "商务负责人",
+          key: "blackValue",
+          minWidth: 200,
+          align: "center"
+        },
+        {
+          title: "操作",
+          key: "action",
+          minWidth: 150,
+          align: "center",
+          render: (h, params) => {
+            return h("div", [
+              h(
+                "Button",
+                {
+                  props: {
+                    type: "primary",
+                    size: "small"
+                  },
+                  style: {
+                    marginRight: "5px"
+                  },
+                  on: {
+                    click: () => {
+                      this.addbusiness ()
+                    }
+                  }
+                },
+                "编辑业务"
+              ),
+              h(
+                "Button",
+                {
+                  props: {
+                    type: "primary",
+                    size: "small"
+                  },
+                  style: {
+                    marginRight: "5px"
+                  },
+                  on: {
+                    click: () => {
+                      
+                    }
+                  }
+                },
+                "渠道报表"
+              ),
+              h(
+                "Button",
+                {
+                  props: {
+                    type: "primary",
+                    size: "small"
+                  },
+                  style: {
+                    marginRight: "5px"
+                  },
+                  on: {
+                    click: () => {
+                      
+                    }
+                  }
+                },
+                "渠道管理"
+              ),
+              h(
+                "Button",
+                {
+                  props: {
+                    type: "error",
+                    size: "small"
+                  },
+                  style: {
+                    marginRight: "5px"
+                  },
+                  on: {
+                    click: () => {
+                      
+                    }
+                  }
+                },
+                "删除业务"
+              )
+
+            ]);
+          }
+        }
+      ],
+      number: "",
+      data6: [
+        {
+          blackType:'华赞金服',
+          blackValue:'haha'
+        }
+        
+
+      ],
+      cityTypel: [],
+      startRow: 1,
+      endRow: 10,
+      total: 0,
+      loading: true,
+      loading3: false
+    };
+  },
+  methods: {
+    // // 待审核
+    pageChange (page) {
+      this.startRow = page
+      this.label_query()
+    },
+    pagesizechange (page) {
+      this.startRow = 1
+      this.endRow = page
+      this.label_query()
+    },
+    changeLoading () {
+      this.loading = false
+      this.$nextTick(() => {
+        this.loading = true
+      })
+    },
+    // 添加编辑供应商
+    handleSubmit (name) {
+      this.$refs[name].validate((valid) => {
+        if (!valid) {
+          return this.changeLoading()
+        } else {     
+          this.$Message.error('Success!')
+        }
+      })
+    },
+    handleReset1 (name) {
+      this.$refs[name].resetFields()
+    },
+    addsupplier () {
+      this.modal9 = true
+
+    },
+    // 删除供应商
+    suppremove () {
+      this.$Modal.confirm({
+          title: '提示',
+          content: '<p>确定要删除吗?</p>',
+          onOk: () => {
+              this.$Message.info('Clicked ok');
+          }
+      });
+    },
+    // 业务
+    // 添加编辑供应商
+    businessSubmit (name) {
+      this.$refs[name].validate((valid) => {
+        if (!valid) {
+          return this.changeLoading()
+        } else {     
+          this.$Message.error('Success!')
+        }
+      })
+    },
+    businessReset (name) {
+      this.$refs[name].resetFields()
+    },
+    addbusiness ()  {
+      this.modal10 = true
+
+    },
+    // 查询
+    label_query(type) {
+      this.loading3 = true;
+      let list =    {
+            pageSize: this.endRow,
+            pageNum: this.startRow,
+            blackValue: this.number ? this.number : null, //手机号or姓名的参数
+          }
+      this.http.post(BASE_URL+"/black/riskBlackList/getRiskBlackListList", list).then(data => {
+        console.log(data)
+        if(data.code == 'success'){
+          // this.data6 = data.data.riskBlackListList;
+          this.total = parseInt(data.data.total);
+          this.loading3 = false;
+        }
+      }).catch(err=>{
+        this.loading3 = false;
+        console.log(err)
+      })
+    },
+    tabshow (num) {
+      let table = document.querySelectorAll('.tabhid')
+      table[num].classList.remove('hidden')
+
+       
+    }
+  },
+  created() {
+    this.label_query()
+  }
+};
+</script>
+<style lang="less" scoped>
+.titletab{
+    width: 100%;
+    border: 1px solid #ccc;
+    border-top: 0;
+    padding: 10px;
+    cursor: pointer;
+}
+.contentcss ul li:first-child .titletab{
+  border-top: 1px solid #ccc;
+}
+.enterprise{
+    display: inline-block;
+    width:40px;
+    height: 25px;
+    line-height: 25px;
+    text-align: center;
+    background: #ccc;
+    color: #fff;
+    border-radius: 5px
+}
+.accountnum{
+   display: inline-block;
+   width:80px;
+   height: 25px;
+   line-height: 25px;
+   text-align: center;
+   background: #1BBC9B;
+   color: #fff;
+   border-radius: 5px 
+}
+
+</style>
