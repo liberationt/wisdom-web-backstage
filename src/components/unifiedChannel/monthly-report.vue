495b0a93788c4106607fccd9fe941107111937aa
冯丹祥 <fengdanxiang@netwisdom.cn>
2018-09-13 10:46:42 +0800

    月报表

--- src/components/unifiedChannel/monthly-report.vue
+++ src/components/unifiedChannel/monthly-report.vue
@@ -11,19 +11,25 @@
           <li>
             <span class="ml20">应用名称:</span>
             <Select v-model="curBusinessCode" placeholder="请选择" style="width:150px" @on-change="businessChange">
-              <Option v-for="item in businessList" :value="item.businessCode" :key="item.businessCode">{{item.businessName }}</Option>
+              <Option v-for="item in businessList" :value="item.businessCode" :key="item.businessCode">
+                {{item.businessName }}
+              </Option>
             </Select>
           </li>
           <li>
             <span class="ml20">供应商:</span>
             <Select v-model="curSuppliersCode" placeholder="请选择" style="width:150px;" @on-change="suppliersChange">
-              <Option v-for="item in suppliersList" :value="item.suppliersCode" :key="item.suppliersCode">{{item.suppliersName }}</Option>
+              <Option v-for="item in suppliersList" :value="item.suppliersCode" :key="item.suppliersCode">
+                {{item.suppliersName }}
+              </Option>
             </Select>
           </li>
           <li>
             <span class="ml20">渠道名称:</span>
             <Select v-model="curChannelCode" placeholder="请选择" style="width:150px;" @on-change="channelChange">
-              <Option v-for="item in channelList" :value="item.suppliersBusinessChannelCode" :key="item.suppliersBusinessChannelCode">{{ item.channelName }}</Option>
+              <Option v-for="item in channelList" :value="item.suppliersBusinessChannelCode"
+                      :key="item.suppliersBusinessChannelCode">{{ item.channelName }}
+              </Option>
             </Select>
           </li>
           <li>
@@ -43,8 +49,7 @@
       <Button class=" ml10" type="primary">导出</Button>
     </div>
     <div id="application_table " class="contentcss mt10">
-      <Table class="tabgrouping" border highlight-row :columns="columns7" :data="data6"></Table>
-      <!-- <Table class="tabgrouping"  :columns="columns7" :data="data6"></Table> -->
+      <Table class="tabgrouping" border highlight-row :columns="reportListColumns" :data="reportList"></Table>
       <div class="tr mt15">
         <Page v-if="startRow!=0" :total="total" :current="startRow" :page-size="endRow" @on-change="pageChange"
               @on-page-size-change="pagesizechange" show-sizer show-total></Page>
@@ -64,7 +69,7 @@
         curBusinessCode: null,
         curSuppliersCode: null,
         curChannelCode: null,
-        curBusinessKey:'',
+        curBusinessKey: '',
         beginTime: '',
         endTime: '',
         account: [],
@@ -72,7 +77,8 @@
         startRow: 1,
         endRow: 10,
 
-        columns7: [
+        reportList: [],
+        reportListColumns: [
           {
             title: '日期',
             key: 'reportDate',
@@ -81,143 +87,128 @@
           },
           {
             title: '供应商',
-            key: 'memberName',
-            minWidth: 120,
+            minWidth: 150,
             align: 'center',
             render: (h, params) => {
-              return h('div', {style: {padding: 0},}, [
-                h('span', {
-                  style: {
-                    display: 'block',
-                    width: '100%',
-                    height: '40px',
-                    lineHeight: '40px',
-                    overflow: 'hidden',
-                    textOverflow: 'ellipsis',
-                    whiteSpace: 'nowrap',
-                    borderBottom: '1px solid #e9eaec'
-                  },
-                  domProps: {
-                    title: params.row.phoneNumber
-                  }
-                }, params.row.phoneNumber),
-                h('span', {
-                  style: {
-                    display: 'block',
-                    width: '100%',
-                    height: '40px',
-                    lineHeight: '40px',
-                    overflow: 'hidden',
-                    textOverflow: 'ellipsis',
-                    whiteSpace: 'nowrap',
-
-                  },
-                  domProps: {
-                    title: params.row.phoneNumber
-                  }
-                }, params.row.phoneNumber)
-              ])
+              return this.reportColumns1Render(h, params.row.suppliersDayReportResList, (suppliers) => {
+                return suppliers.suppliersName
+              })
             }
 
           },
           {
             title: '渠道',
+            minWidth: 200,
             align: 'center',
-            minWidth: 80,
             render: (h, params) => {
-              let realStatus
-              if (params.row.realStatus == 0) {
-                realStatus = '未实名'
-              } else if (params.row.realStatus == 1) {
-                realStatus = params.row.realName
-              }
-              return h('div', [
-                h('span', {}, realStatus)
-              ])
+              return this.reportColumns2Render(h, params.row.suppliersDayReportResList, (report) => {
+                return report.channelName
+              });
             }
           },
           {
             title: '折扣系数',
-            key: 'channelCode',
-            minWidth: 120,
-            align: 'center'
-          },
-          {
-            title: 'PV',
-            key: 'channelCode',
-            minWidth: 120,
-            align: 'center'
-          },
-          {
-            title: 'UV',
-            key: 'channelCode',
-            minWidth: 120,
-            align: 'center'
-          },
-          {
-            title: '注册',
-            key: 'channelCode',
-            minWidth: 120,
-            align: 'center'
-          },
-          {
-            title: '注册转化率',
-            key: 'channelCode',
-            minWidth: 120,
-            align: 'center'
-          },
-          {
-            title: '累计激活',
-            key: 'channelCode',
-            minWidth: 120,
-            align: 'center'
-          },
-          {
-            title: '累计激活转化率',
-            key: 'object',
+            minWidth: 70,
             align: 'center',
-            minWidth: 80,
             render: (h, params) => {
-              let accountStatus
-              if (params.row.accountStatus == 0) {
-                accountStatus = '冻结'
-              } else if (params.row.accountStatus == 1) {
-                accountStatus = '正常'
-              }
-              return h('div', [
-                h('span', {}, accountStatus)
-              ])
+              return this.reportColumns2Render(h, params.row.suppliersDayReportResList, (report) => {
+                return report.discountFact + '%'
+              });
             }
           },
           {
-            title: '累计申请',
-            key: 'channelCode',
-            minWidth: 120,
-            align: 'center'
+            title: 'PV',
+            minWidth: 100,
+            align: 'center',
+            render: (h, params) => {
+              return this.reportColumns2Render(h, params.row.suppliersDayReportResList, (report) => {
+                return report.pv
+              });
+            }
           },
           {
-            title: '累计申请转化率',
-            key: 'channelCode',
-            minWidth: 120,
-            align: 'center'
+            title: 'UV',
+            minWidth: 100,
+            align: 'center',
+            render: (h, params) => {
+              return this.reportColumns2Render(h, params.row.suppliersDayReportResList, (report) => {
+                return report.uv
+              });
+            }
           },
-
+          {
+            title: '注册',
+            minWidth: 120,
+            align: 'center',
+            render: (h, params) => {
+              return this.reportColumns2Render(h, params.row.suppliersDayReportResList, (report) => {
+                return report.registerCount + '(' + report.discountRegisterCount + ')'
+              });
+            }
+          },
+          {
+            title: '注册转化率',
+            minWidth: 70,
+            align: 'center',
+            render: (h, params) => {
+              return this.reportColumns2Render(h, params.row.suppliersDayReportResList, (report) => {
+                return report.registerRate + "%"
+              });
+            }
+          },
+          {
+            title: '激活',
+            minWidth: 120,
+            align: 'center',
+            render: (h, params) => {
+              return this.reportColumns2Render(h, params.row.suppliersDayReportResList, (report) => {
+                return report.activeCount + '(' + report.discountActiveCount + ')'
+              });
+            }
+          },
+          {
+            title: '激活转化率',
+            minWidth: 70,
+            align: 'center',
+            render: (h, params) => {
+              return this.reportColumns2Render(h, params.row.suppliersDayReportResList, (report) => {
+                return report.activeRate + "%"
+              });
+            }
+          },
+          {
+            title: '申请',
+            minWidth: 120,
+            align: 'center',
+            render: (h, params) => {
+              return this.reportColumns2Render(h, params.row.suppliersDayReportResList, (report) => {
+                return report.applyCount
+              });
+            }
+          },
+          {
+            title: '申请转化率',
+            minWidth: 70,
+            align: 'center',
+            render: (h, params) => {
+              return this.reportColumns2Render(h, params.row.suppliersDayReportResList, (report) => {
+                return report.applyRate + "%"
+              });
+            }
+          },
         ],
-        reportList: []
       }
     },
     methods: {
       // 分页
       pageChange(page) {
-        // console.log(page)
         this.startRow = page
-        this.inquire()
+        this.queryReportList()
       },
       pagesizechange(page) {
-        // console.log(page)
         this.startRow = 1
         this.endRow = page
-        this.inquire()
+        this.queryReportList()
       },
       rowClassName(row, index) {
         if (index == 0) {
@@ -226,6 +217,55 @@
           return 'demo-table-info-cell-name';
         }
         return ''
+      },
+
+      reportColumns1Render(h, params, showTextCallback) {
+        let border = '1px solid #e9eaec'
+        let list = []
+        for (let i = 0; i < params.length; i++) {
+          if (params.length == 1) {
+            border = '0px'
+          }
+          let text = showTextCallback ? showTextCallback(params[i]) : params[i]
+          list.push(
+            h('span', {
+              style: {
+                display: 'block',
+                width: '100%',
+                height: '40px',
+                lineHeight: '40px',
+                borderBottom: border
+              }
+            }, text)
+          )
+        }
+        return h('div', list)
+      },
+
+      reportColumns2Render(h, params, showTextCallback) {
+        let border = '1px solid #e9eaec'
+        let list = []
+        for (let i = 0; i < params.length; i++) {
+          for (let j = 0; j < params[i].channelReportList.length; j++) {
+            if (params[i].channelReportList.length - 1 == j) {
+              border = '0px'
+            }
+
+            let text = showTextCallback ? showTextCallback(params[i].channelReportList[j]) : params[i].channelReportList[j]
+            list.push(
+              h('span', {
+                style: {
+                  display: 'block',
+                  width: '100%',
+                  height: '40px',
+                  lineHeight: '40px',
+                  borderBottom: border
+                }
+              }, text)
+            )
+          }
+        }
+        return h('div', list)
       },
 
       // 时间选择
@@ -247,14 +287,14 @@
         return y + "-" + m + "-" + d;
       },
       //查询业务列表
-      queryBusiness() {
+      queryBusiness(callback) {
         this.http.post(BASE_URL + '/promotion/business/queryListByUserCode', {}).then((resp) => {
           if (resp.code == 'success') {
             this.businessList = resp.data
-            if(this.businessList && this.businessList.length > 0){
+            if (this.businessList && this.businessList.length > 0) {
               this.curBusinessCode = resp.data[0].businessCode
+              callback && callback()
             }
-            this.queryReportList();
           }
         }).catch(() => {
         })
@@ -262,7 +302,7 @@
       // 选择业务后查询供应商
       businessChange(businessCode) {
         this.curBusinessCode = businessCode
-        this.suppliersList= []
+        this.suppliersList = []
         this.channelList = []
         this.http.post(BASE_URL + '/promotion/suppliersBusiness/queryListByBusinessCode', {
           businessCode: businessCode
@@ -288,22 +328,21 @@
         })
       },
       //选择渠道后
-      channelChange(channelCode){
+      channelChange(channelCode) {
         this.curChannelCode = channelCode;
       },
       // 列表查询
       queryReportList() {
-        this.loading3 = true
         let date1 = Date.parse(new Date(this.beginTime)) / 1000
         let date2 = Date.parse(new Date(this.endTime)) / 1000
         if (date1 > date2) {
-          this.loading3 = false
           this.$Modal.warning({
             title: '提示',
             content: '<p>开始时间不得大于结束时间</p>'
           })
           return false
         }
+
         let params = {
           startDate: this.beginTime,
           endDate: this.endTime,
@@ -311,14 +350,14 @@
           suppliersCode: this.curSuppliersCode,
           suppliersBusinessChannelCode: this.curChannelCode
         }
+        this.loading3 = true
         this.http.post(BASE_URL + '/promotion/suppliersBusinessReport/querySuppliersMonthReport', params).then((resp) => {
           this.loading3 = false
-            if (resp.code == 'success') {
-              this.reportList = resp.data
-            }
-          }).catch(() => {
-            this.loading3 = false
-          })
+          this.reportList = resp.data
+        }).catch(() => {
+          this.loading3 = false
+          this.reportList = []
+        })
       },
       // 导出
       exports() {
@@ -339,11 +378,13 @@
     },
     mounted() {
       // 获取当前时间
-      var date=new Date();
-      this.endTime = this.timeFormat(date,1)
+      var date = new Date();
+      this.endTime = this.timeFormat(date, 1)
       date.setDate(1);
-      this.beginTime = this.timeFormat(date,0)
-      this.queryBusiness()
+      this.beginTime = this.timeFormat(date, 0)
+      this.queryBusiness(() => {
+        this.queryReportList();
+      })
     }
   }
 </script>
